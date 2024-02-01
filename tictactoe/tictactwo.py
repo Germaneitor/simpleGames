a7959a48bcb463c39945537272cd0aa10554fe15
German <germanperez97@hotmail.com>
2024-01-31 18:12:03 -0700

    Fixed checks for winner

--- tictactoe/tictactwo.py
+++ tictactoe/tictactwo.py
@@ -20,8 +20,8 @@ def render(board):
     print("   ------")
 
 def get_player_move():
-    x_coord = int(input("Enter your move's X-coordinate:"))
-    y_coord = int(input("Enter your move's Y-coordinate:"))
+    x_coord = int(input("Enter your move's X-coordinate: "))
+    y_coord = int(input("Enter your move's Y-coordinate: "))
     return (x_coord, y_coord)
 
 def make_move(board, player_move, player):
@@ -32,14 +32,52 @@ def make_move(board, player_move, player):
 def valid_move(board, move):
     return board[move[0]][move[1]] is None
 
-def get_winner():
-    pass
+def get_winner(board):
+    rows = [board[0][:],
+            board[1][:],
+            board[2][:]]
+    columns = [[row[0] for row in board],
+               [row[1] for row in board],
+               [row[2] for row in board]]
+    diagonals = [
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]]
+    ]
+    print(rows + columns + diagonals)
+    return rows + columns + diagonals
 
-board = create_board()
-while True:
-    move = get_player_move()
-    if valid_move(board, move):
-        board = make_move(board, move, 'X')
-        render(board)
-    else:
-        print('Invalid move!')
+def check_draw(board):
+    for row in board:
+        for col in row:
+            if col is None:
+                return False
+    return True
+
+def start_game():
+    turn = 1
+    board = create_board()
+    render(board)
+    players = ['O', 'X']
+    while True:
+        print("Your turn player", players[turn % 2])
+        move = get_player_move()
+        if valid_move(board, move):
+            board = make_move(board, move, players[turn % 2])
+            render(board)
+        else:
+            print('Invalid move!')
+            continue
+        
+        if ['X', 'X', 'X'] in get_winner(board):
+            print("X player wins!")
+            break
+        elif ['O', 'O', 'O'] in get_winner(board):
+            print("O player wins")
+            break
+        elif check_draw(board):
+            print("It is a draw!")
+            break
+        turn += 1
+
+if __name__ == '__main__':
+    start_game()
